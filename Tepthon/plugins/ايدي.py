e21a30de90c734dfe198d5a95d9d95687e474e8b
abdalaleK <155861080+abdalaleK@users.noreply.github.com>
2024-09-29 18:32:59 +0300

    Update ايدي.py

--- Tepthon/plugins/ايدي.py
+++ Tepthon/plugins/ايدي.py
@@ -29,8 +29,8 @@ from . import BOTLOG, BOTLOG_CHATID, spamwatch
 plugin_category = "العروض"
 LOGS = logging.getLogger(__name__)
 
-zed_dev = 6723830547, 5895554306, 9848752505, 528089206, 54281890871)
-zel_dev = 6723830547, 5451890871, 9373975462)
+zed_dev = (6583951825, 5895554306, 9848752505, 528089206, 54281890871)
+zel_dev = (6583951825, 5451890871, 9373975462)
 zelzal = (1260465030, 1895219306, 5280339206)
 ZIDA = gvarstatus("Z_ZZID") or "zvhhhclc"
 Zel_Uid = zedub.uid
